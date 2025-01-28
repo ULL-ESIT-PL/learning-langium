f016aaade893887f504f70dc82989048760f2f38
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-01-28 15:14:21 +0000

    reproduced part-4 from the video without copying

--- src/language/hello-world-validator.ts
+++ src/language/hello-world-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { HelloWorldAstType, Person } from './generated/ast.js';
+import type { HelloWorldAstType, Model, Def } from './generated/ast.js';
 import type { HelloWorldServices } from './hello-world-module.js';
 
 /**
@@ -9,7 +9,8 @@ export function registerValidationChecks(services: HelloWorldServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.HelloWorldValidator;
     const checks: ValidationChecks<HelloWorldAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        Model: validator.checkModel,
+        Def: validator.checkDef,
     };
     registry.register(checks, validator);
 }
@@ -19,13 +20,44 @@ export function registerValidationChecks(services: HelloWorldServices) {
  */
 export class HelloWorldValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    /**
+     * Check model for duplicated def names
+     * 
+     * @param model 
+     * @param accept 
+     */
+    checkModel(model: Model, accept: ValidationAcceptor): void {
+
+        // Check for duplicated def names
+        let defs = model.defs;
+        let defNames = new Set<string>();
+        for (let def of defs) {
+            if (defNames.has(def.name)) {
+                accept('error', 'Duplicate def name.', { node: def, property: 'name' });
+            } else {
+                defNames.add(def.name);
             }
         }
     }
 
+    /**
+     * Check definitions for duplicated params
+     * 
+     * @param def 
+     * @param accept 
+     */
+    checkDef(def: Def, accept: ValidationAcceptor): void {
+        const params = def.params;
+        const paramNames = new Set<string>();
+        for (let param of params) {
+            if (paramNames.has(param.name)) {
+                accept('error', `Duplicate parameter name. '${param.name}'`, { node: param, property: 'name' });
+            } else {
+                paramNames.add(param.name);
+            }
+        }
+
+    }
+
 }
+
